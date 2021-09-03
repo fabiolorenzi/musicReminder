754737da921945d9b422b537d332a546d17b4573
fabiolorenzi <fabiolorenzi@gmail.com>
2021-09-03 21:36:22 +0100

    created basic table in list component

--- client/src/app/components/list/list.component.ts
+++ client/src/app/components/list/list.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
+
+import { SongService } from "../../services/song.service";
+import { Song } from "../../services/song.model";
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
+  providers: [SongService]
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  constructor(public songService: SongService) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit() {
+    this.refreshData();
+  };
+
+  refreshData() {
+    this.songService.getSongsList().subscribe(res => {
+      this.songService.songs = res as Song[];
+    });
+  };
 
 }
