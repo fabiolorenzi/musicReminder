e5ed6fb1a1d154144770800d3119db6d8d38d044
fabiolorenzi <fabiolorenzi@gmail.com>
2021-09-03 19:06:22 +0100

    setted song service

--- client/src/app/services/song.service.ts
+++ client/src/app/services/song.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Song } from "./song.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SongService {
+  selectedSong!: Song;
+  songs!: Song[];
+  readonly baseUrl = "http://localhost:8082/songs";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getSongsList() {
+    return this.http.get(this.baseUrl);
+  };
+
+  postSong(song: Song) {
+    return this.http.post(this.baseUrl, song);
+  };
+
+  putSong(song: Song) {
+    return this.http.put(this.baseUrl + `/${song._id}`, song);
+  };
+
+  deleteSong(_id: string) {
+    return this.http.delete(this.baseUrl + '/${_id}');
+  };
 }
