4724cae84a1f326b6e480e48bce825a7927d41f5
fabiolorenzi <fabiolorenzi@gmail.com>
2021-09-04 08:45:48 +0100

    setted show-form service

--- client/src/app/services/show-form.service.ts
+++ client/src/app/services/show-form.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShowFormService {
+  private showUpdateForm: boolean = false;
+  private subject = new Subject<any>();
 
   constructor() { }
+
+  toggleShowUpdateForm(): void {
+    this.showUpdateForm = !this.showUpdateForm;
+    this.subject.next(this.showUpdateForm);
+  };
+
+  onToggle(): Observable<any> {
+    return this.subject.asObservable();
+  };
 }
