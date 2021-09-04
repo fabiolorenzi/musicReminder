1f28a57a280b00c0510703e59ab1a9322ed8cff5
fabiolorenzi <fabiolorenzi@gmail.com>
2021-09-04 09:08:41 +0100

    created logic to show and hide the update form

--- client/src/app/components/list/list.component.ts
+++ client/src/app/components/list/list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from "@angular/forms";
+import { ShowFormService } from "../../services/show-form.service";
+import { Subscription } from "rxjs";
 
 import { SongService } from "../../services/song.service";
 import { Song } from "../../services/song.model";
@@ -11,12 +13,24 @@ import { Song } from "../../services/song.model";
   providers: [SongService]
 })
 export class ListComponent implements OnInit {
+  subscription!: Subscription;
+  showUpdate!: boolean;
+  showUpdateForm: boolean = false;
 
-  constructor(public songService: SongService) { }
+  constructor(public songService: SongService, private showFormService: ShowFormService) {
+    this.subscription = this.showFormService.onToggle().subscribe(value => this.showUpdateForm = value);
+  };
 
   ngOnInit() {
     this.resetter();
     this.refreshData();
+  };
+
+  toggleShowUpdater() {
+    this.showFormService.toggleShowUpdateForm();
+    if (!this.showUpdateForm) {
+      window.location.reload();
+    };
   };
 
   resetter(form?: NgForm) {
@@ -55,6 +69,7 @@ export class ListComponent implements OnInit {
       alert("The song has been updated successfully!");
       this.resetter(form);
       this.refreshData();
+      this.toggleShowUpdater();
     });
   };
 
